5d086e2250b0f47a3e5987c30c056ac2901ac047
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2023-12-20 00:52:03 +0600

    header, footer updated and backToTop button added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 //Sticky Header
-window.addEventListener("scroll", function () {
-  const header = document.querySelector(".header");
-  header.classList.toggle("sticky", window.scrollY > 0);
-});
+// window.addEventListener("scroll", function () {
+//   const header = document.querySelector(".header");
+//   header.classList.toggle("sticky", window.scrollY > 0);
+// });
 
 //Mobile Navigation
 const navigation = document.querySelector(".navigation");
@@ -11,8 +11,22 @@ document.querySelector(".toggle").onclick = function () {
   navigation.classList.toggle("active");
 };
 
-const mobileNavigation = document.querySelector(".mobile-submenu-item");
 document.querySelector(".mobile-submenu-item").onclick = function () {
   this.classList.toggle("mSactive");
-  navigation.classList.toggle("mSactive");
+};
+
+//Back to Top
+const backToTopButton = document.querySelector(".back-to-top");
+backToTopButton.onclick = () =>
+  document.documentElement.scroll({
+    top: 0,
+    behavior: "smooth",
+});
+
+window.onscroll = () => {
+  if (document.documentElement.scrollTop > 200) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
 };
